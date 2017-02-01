6d8b3d609b0cdb19adcd98bc3150d98072a856df
Lena Gritsuk <lenagritsuk@inbox.ru>
2017-02-02 00:38:00 +0300

    first version

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,39 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.first=0;
+    	this.last=0;
+    	this.solution=0;
+    }
 
     setRange(min, max) {
+    	if (min<max) {
+    	this.first=min;
+    	this.last=max;
 
+    	this.solution= Math.round(this.first + (this.last-this.first)/2);
+    	console.log("setRange:",this.first,this.last,this.solution);
+     }
     }
 
     guess() {
+    	console.log("guess:",this.first,this.last,this.solution);
+    	return this.solution;
 
     }
 
     lower() {
-
+    	this.last=this.solution;
+    	this.solution=Math.round(this.first + (this.last-this.first)/2);
+    	console.log("lower",this.first,this.last,this.solution);
+    	return this.solution;
     }
 
     greater() {
-
+    	
+    	this.first=this.solution;
+    	this.solution=Math.round(this.first+(this.last-this.first)/2);
+    	console.log("lower",this.first,this.last,this.solution);
+    	return this.solution;
     }
 }
 
